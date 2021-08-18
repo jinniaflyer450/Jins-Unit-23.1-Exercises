25c8dfb17e4c420106ed7d6de2967b6a9f9a3003
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-08-18 10:39:46 -0500

    started /users/new get and post routes

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -13,3 +13,24 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
 db.create_all()
+
+@app.route('/users')
+def show_users():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+@app.route('/users/new')
+def add_user_form():
+    return render_template('adduser.html')
+
+@app.route('/users/new', methods=['POST'])
+def add_user():
+    username = request.form['username']
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    img_url = request.form['img-url']
+    new_user = User(username=username, first_name=first_name, last_name=last_name, image_url = img_url)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/users')
+
