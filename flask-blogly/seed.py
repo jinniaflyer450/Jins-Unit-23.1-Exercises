9ea457be38baf5be184638ccdd6398af1c952d6c
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-09-04 11:41:31 -0500

    updated seed file with default posts

--- flask-blogly/seed.py
+++ flask-blogly/seed.py
@@ -14,9 +14,14 @@ def insert_data():
     alice = User(username='l33thacks', first_name='Alice', last_name = 'Johnson')
     bob = User(username='codingiscool30', first_name = 'Bob', last_name='Wu')
     cate= User(username='miraclewoman', first_name='Cate', last_name='Merriweather')
-    db.session.add(alice)
-    db.session.add(bob)
-    db.session.add(cate)
+    db.session.add_all([alice, bob, cate])
+    db.session.commit()
+
+    alice_post1 = Post(title='L33t hackz', content='Here are some l33t hacks--turn it off and back on.', user_id=1)
+    bob_post1 = Post(title='Coding is cool!', content="I love coding because it's cool!", user_id=2)
+    cate_post1 = Post(title='How to get rid of stalkers?', content="I have a stalker. Help?", user_id=3)
+    cate_post2 = Post(title='Never mind...', content='I have bigger problems now.', user_id=3)
+    db.session.add_all([alice_post1, bob_post1, cate_post1, cate_post2])
     db.session.commit()
 
 reset_database()
